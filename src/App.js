ba1f480386d89533a6f40f889191079334a75fbe
Murad Sarker <sarker24@gmail.com>
2022-03-25 19:24:53 +0100

    Add product json file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import Nav from "./components/Nav/Nav";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
       <Nav />
+      <Header />
     </div>
   );
 }
