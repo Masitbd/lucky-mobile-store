bf30010ee731b0150c657f223781b7d6b9e585a0
Murad Sarker <sarker24@gmail.com>
2022-03-25 20:32:32 +0100

    Add product details

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./Product.css";
 
 const Product = ({ product }) => {
-  const { id, name, seller, price, stock, img } = product;
+  const { name, seller, price, stock, img } = product;
 
   return (
     <div className="product-container">
       <img src={img} alt="" />
+      <p className="item-text">Name: {name}</p>
+      <p className="item-text">Brand: {seller}</p>
+      <p className="item-text">Price: ${price}</p>
+      <p className="item-text">Stock: {stock}</p>
     </div>
   );
 };
