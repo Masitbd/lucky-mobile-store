731777d6b4d8679d8add4a69fbac223e750f444e
Murad Sarker <sarker24@gmail.com>
2022-03-26 00:22:25 +0100

    Add trash button

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./Cart.css";
+import { FaCartPlus, FaRemoveFormat, FaTrash } from "react-icons/fa";
 
 const Cart = ({ cart }) => {
   return (
     <div className="cart">
-      {cart.map((item) => (
-        <p key={item.id}>{item.name}</p>
-      ))}
+      <h3>Added item: {cart.length}</h3>
+
+      {cart.length !== 4 &&
+        cart.map((item) => {
+          return (
+            <div className="cart-item-list">
+              <p key={item.id}>{item.name}</p>
+              <button className="trash-button">
+                <FaTrash />
+              </button>
+            </div>
+          );
+        })}
     </div>
   );
 };
